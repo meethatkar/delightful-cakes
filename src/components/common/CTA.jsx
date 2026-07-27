d10b856bbd57e916b9ad29dfaf1809082571e9f2
meethatkar <meethatkar000@gmail.com>
2026-07-27 17:29:18 +0530

    refactor: import CTA bread image as a module instead of hardcoded path

--- src/components/common/CTA.jsx
+++ src/components/common/CTA.jsx
@@ -1,12 +1,13 @@
 import Button from "../ui/Button"
 import Container from "../ui/Container"
 import Section from "../ui/Section"
+import breadImg from "../../assets/images/cta/bread.webp"
 
 const CTA = () => {
   return (
     <Container>
       <Section className={"relative"}>
-        <img src="src/assets/images/cta/bread.webp" alt="" className="w-full md:w-9/12 max-w-5xl mx-auto h-[50vh] object-cover rounded-xl" />
+        <img src={breadImg} alt="" className="w-full md:w-9/12 max-w-5xl mx-auto h-[50vh] object-cover rounded-xl" />
         <div className="text-div absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-3 items-center justify-center text-center w-full max-w-5xl bg-black/40 h-[50vh] rounded-xl">
           <span className="text-5xl md:text-8xl font-extrabold font-cta tracking-wider text-transparent [-webkit-text-stroke:0.1px_white]">
             Crafted To Perfection
